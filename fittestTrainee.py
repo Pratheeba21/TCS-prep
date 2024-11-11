5c80345343f3ddae71a70bea64df3485d52a15c8
Pratheeba S <pratheebas887@gmail.com>
2024-11-11 12:11:36 +0530

    fittestTrainee.py
    Signed-off-by: Pratheeba S <pratheebas887@gmail.com>

---
+++ fittestTrainee.py
@@ -0,0 +1,29 @@
+t1,t2,t3=list(),list(),list()
+
+for _ in range(3):
+    x=int(input())
+    if x not in range(1,101):
+        print("Invalid input")
+        break
+    t1.append(x)
+    x=int(input())
+    if x not in range(1,101):
+        print("Invalid input")
+        break
+    t2.append(x)
+    x=int(input())
+    if x not in range(1,101):
+        print("Invalid input")
+        break
+    t3.append(x)
+
+m=[sum(t1)//3, sum(t2)//3, sum(t3)//3]
+maxFit=max(m)
+
+if maxFit<70 :
+    print("All trainees are unfit")
+    
+
+for i in range(3):
+    if m[i]==maxFit:
+        print(f"Trainee number: {i+1} , Average oxygen level: {m[i]}")
